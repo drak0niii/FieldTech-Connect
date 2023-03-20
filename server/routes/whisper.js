d6aeb9831d1db231f7a04a7451beae207fd9f999
drak0niii <icostiin@icloud.com>
2023-03-20 23:32:34 +0200

    Update whisper.js

--- server/routes/whisper.js
+++ server/routes/whisper.js
@@ -9,7 +9,7 @@ const upload = multer({ storage });
 
 // Configure the Whisper ASR API URL and API key
 const WHISPER_API_URL = "https://api.openai.com/v1/audio/transcriptions";
-const WHISPER_API_KEY = "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";
+const WHISPER_API_KEY = process.env.WHISPER_API_KEY;
 
 router.post("/transcribe", upload.single("audio"), async (req, res) => {
   try {
@@ -17,7 +17,10 @@ router.post("/transcribe", upload.single("audio"), async (req, res) => {
     console.log("audioBuffer:", audioBuffer);
     const response = await axios.post(
       WHISPER_API_URL,
-      { data: audioBuffer.toString("base64") },
+      { 
+        model: "whisper-1",
+        data: audioBuffer.toString("base64")
+      },
       {
         headers: {
           "Content-Type": "application/octet-stream",
@@ -31,6 +34,9 @@ router.post("/transcribe", upload.single("audio"), async (req, res) => {
   } catch (error) {
     console.error("Whisper API error:", error.response ? JSON.stringify(error.response.data, null, 2) : error);
     console.error("Full error object:", JSON.stringify(error, null, 2));
+    console.error("Request headers:", JSON.stringify(req.headers, null, 2));
+    console.error("Request body:", JSON.stringify(req.body, null, 2));
+    console.error("Request file:", req.file);
     res.status(500).json({ error: "Failed to transcribe audio" });
   }
 });
